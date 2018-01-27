8a7e732c4281902007f89c260a8035fb69ad073a
cmstuning <cmstuning@gmail.com>
2018-01-27 12:14:17 +0200

    added libs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+// const Web3 = require('web3');
+import Web3 from 'web3';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  coinbase;
+  balance;
+
+  constructor() {
+    console.log('web3 loaded', this.web3)
+    // console.log('coinbase', this.web3.eth.coinbase)
+    // console.log('coinbase', this.web3.eth.getBalance(coinbase);
+  }
 }
